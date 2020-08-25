955ce62620192ee236dc5355cad9366666258100
Derek Teed <Derek@MacBook-Air.local>
2020-08-25 13:42:23 -0500

    update links/navbar

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,13 @@
-export default () => ( <div>Hello Next.js here</div> );
+import Layout from '../components/Layout.js'
+
+const Index = () => (
+    <Layout>
+        <div>
+
+            <h1>Welcome to CAN SLIM Investing System</h1>
+            <h2>Where we do the work for you!</h2>
+        </div>
+    </Layout>
+)
+
+export default Index;
