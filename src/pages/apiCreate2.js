00f67d163f668e55c0c47e0fa668fe90792ab76e
Derek Teed <Derek@MacBook-Air.local>
2020-09-07 22:54:55 -0500

    added to DB

--- src/pages/apiCreate2.js
+++
@@ -1,9 +0,0 @@
-import React from 'React';
-import fetch from 'isomorphic-fetch';
-import Facts from 
-
-export default function apiCreate2() {
-    return (
-        <div> Hello React</div>
-    )
-}
