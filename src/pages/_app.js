1ca0fc0790a91d671c5e12cb0b2a3f94918b94f0
Derek Teed <Derek@MacBook-Air.local>
2020-09-21 21:14:54 -0500

    better table

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 //import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../css/App.css'
 
 export default function MyApp({ Component, Props }) {
   return <Component {...Props} />
